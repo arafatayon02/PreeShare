a6ce345029a126eafccbbc384530fe605084b074
arafatayon02 <arafatrahman133a@gmail.com>
2026-05-02 00:05:45 +0600

    building structare

--- peershare/marketplace/models.py
+++ peershare/marketplace/models.py
@@ -31,6 +31,7 @@ class Review(models.Model):
     user       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     rating     = models.IntegerField(choices=RATING_CHOICES)
     comment    = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.user} → {self.item} ({self.rating}★)'
